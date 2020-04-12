a5f4a04fc50703178ed2c80569f95b3f80ab8c38
pashishsharma385 <53710102+pashishsharma385@users.noreply.github.com>
2020-04-12 13:36:43 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,51 @@
+package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+import dao.CityDAO;
+import dao.TeamDAO;
+import model.City;
+import model.Team;
+
+public class Main 
+
+{
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		CityDAO citydao=new CityDAO();
+		TeamDAO teamdao=new TeamDAO();
+		
+		System.out.println("Enter City Name");
+		String cityName=br.readLine();
+		
+		System.out.println("Enter id");
+		long id=Long.parseLong(br.readLine());
+		
+		City city=new City(id,cityName);
+		
+		System.out.println("Enter Name");
+		String name=br.readLine();
+		System.out.println("Enter Coach Name");
+		String CoachName=br.readLine();	
+		System.out.println("Enter Captain Name");
+		String CaptianName=br.readLine();
+		Team team=new Team(name,CoachName,CaptianName,city);
+		
+		teamdao.createTeam(team);
+		
+		System.out.println(" Enter team name to get deatils of cityname and cityID");
+		
+		String check=br.readLine();
+	   city=	citydao.getCityByName(check);
+	
+	System.out.println("City id = "+city.getCityId());
+	System.out.println("city name = "+city.getCityName());
+		
+	}
+	
+}
--- src/model/Team.java
+++ src/model/Team.java
@@ -1,1 +1,54 @@
+package model;
 
+public class Team<City>
+{
+private String	name;
+private  String coach;
+private String captain;
+
+private City city;
+
+
+public String getCaptain() {
+	return captain;
+}
+
+public void setCaptain(String captain) {
+	this.captain = captain;
+}
+
+public Team(String name, String coach, String captain, City city) {
+	super();
+	this.name = name;
+	this.coach = coach;
+	this.captain = captain;
+	this.city = city;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+public City getCity() {
+	return city;
+}
+
+public void setCity(City city) {
+	this.city = city;
+}
+
+public String getCoach() {
+	return coach;
+}
+
+public void setCoach(String coach) {
+	this.coach = coach;
+}
+
+
+
+
+}
